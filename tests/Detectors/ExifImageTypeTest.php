c3182e0a85d6e3228ec0ff430e7e8eceae6c702d
rosell-dk <it@rosell.dk>
2019-03-15 12:11:40 +0100

    BaseDetectorTester is not a Test class...

--- tests/Detectors/ExifImageTypeTest.php
+++ tests/Detectors/ExifImageTypeTest.php
@@ -4,6 +4,8 @@ namespace Tests\ImageMimeTypeGuesser\Detectors;
 use \Tests\ImageMimeTypeGuesser\Detectors\BaseDetectorTester;
 use \PHPUnit\Framework\TestCase;
 
+include_once 'BaseDetectorTester.php';  // Not autoloaded, because it does not end with "Test"
+
 class ExifImageTypeTest extends TestCase
 {
     public function testDoDetect()
--- tests/Detectors/FInfoTest.php
+++ tests/Detectors/FInfoTest.php
@@ -4,6 +4,8 @@ namespace Tests\ImageMimeTypeGuesser\Detectors;
 use \Tests\ImageMimeTypeGuesser\Detectors\BaseDetectorTester;
 use \PHPUnit\Framework\TestCase;
 
+include_once 'BaseDetectorTester.php';  // Not autoloaded, because it does not end with "Test"
+
 class FInfoTest extends TestCase
 {
     public function testDoDetect()
