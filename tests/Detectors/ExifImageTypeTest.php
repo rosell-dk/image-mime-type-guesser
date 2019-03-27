961a2a8c0d378fc8333b8af94577dcb83e25d807
rosell-dk <it@rosell.dk>
2019-03-27 08:59:36 +0100

    The `image_type_to_mime_type` call was done without first testing if that function exists. This resulted in a testcase failure #2

--- tests/Detectors/ExifImageTypeTest.php
+++ tests/Detectors/ExifImageTypeTest.php
@@ -17,7 +17,7 @@ class ExifImageTypeTest extends TestCase
     {
         if (function_exists('image_type_to_mime_type')) {
             $this->assertEquals(18, IMAGETYPE_WEBP);
+            $this->assertEquals('image/webp', image_type_to_mime_type(IMAGETYPE_WEBP));
         }
-        $this->assertEquals('image/webp', image_type_to_mime_type(IMAGETYPE_WEBP));
     }
 }
