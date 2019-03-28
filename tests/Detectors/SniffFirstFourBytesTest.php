8eb1991decf47645b2ca14d17f164732d873552e
rosell-dk <it@rosell.dk>
2019-03-28 09:37:28 +0100

    Added new detector that can detect png, gif and jpeg by inspecting the first four bytes. Closes #5

---
+++ tests/Detectors/SniffFirstFourBytesTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Tests\ImageMimeTypeGuesser\Detectors;
+
+use \Tests\ImageMimeTypeGuesser\Detectors\AbstractDetectorTester;
+use \PHPUnit\Framework\TestCase;
+
+include_once 'AbstractDetectorTester.php';  // Not autoloaded, because it does not end with "Test"
+
+class SniffFirstFourBytesTest extends TestCase
+{
+    public function testDoDetect()
+    {
+        AbstractDetectorTester::testDetect($this, 'SniffFirstFourBytes');
+    }
+}
